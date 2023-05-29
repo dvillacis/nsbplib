642816f5b009a8064d2b863da873be237fda4462
David <david@villacis.net>
2023-05-29 13:31:50 -0500

    updated L2 operator for general forward models

--- nsbplib/operators/SDL2.py
+++ nsbplib/operators/SDL2.py
@@ -1,21 +1,34 @@
 import numpy as np
+from scipy.sparse.linalg import cg
+from pylops import Identity, LinearOperator, Diagonal
 from pyproximal.ProxOperator import _check_tau
 from pyproximal import ProxOperator
 
 class SDL2(ProxOperator):
-    def __init__(self, b, sigma:np.array):
+    def __init__(self, b, sigma:np.array, A:LinearOperator=None):
         super().__init__(None,True)
         self.b = b
         self.sigma = sigma
+        self.A = A
+        if A == None:
+            self.A = Identity(N=b.shape[0])
+        self.ATb = self.A.T * self.b
+        self.ATA = self.A.T * self.A
 
     def __call__(self, x):
-        return 0.5 * np.dot(self.sigma,(x-self.b)**2)
+        return 0.5 * np.dot(self.sigma,(self.A*x-self.b)**2)
 
     @_check_tau
     def prox(self,x,tau):
-        num = x + tau * self.sigma * self.b
-        x = num / (1. + tau * self.sigma)
+        rhs = x + tau * Diagonal(self.sigma) * self.ATb
+        lhs = Identity(N=x.shape[0]) + tau * Diagonal(self.sigma) * self.ATA
+        # print(f'{type(rhs)=} {type(lhs)=}')
+        # print(f'{rhs.shape=} {lhs.shape=}')
+        x,exit_code = cg(lhs,rhs)
+        if exit_code != 0:
+            raise ValueError(f'Conjugate gradient for calculating proximal did not converge: {exit_code}')
         return x
 
     def grad(self,x):
-        g = self.sigma * (x - self.b)
+        g = self.sigma * self.A.T*(self.A*x - self.b)
+        return g
--- nsbplib/solvers/rof/solver.py
+++ nsbplib/solvers/rof/solver.py
@@ -51,3 +51,19 @@ class ROFSolver_2D(object):
         mu = 1.0 / (tau*self.L)
         rec = PrimalDual(l2,l21,self.K,tau=tau,mu=mu,theta=1.0,x0=np.zeros_like(self.data.ravel()),niter=1000)
         return rec.reshape(self.data.shape)
+    
+class TVInpainting(object):
+    def __init__(self,data,K,R) -> None:
+        self.data = data
+        self.K = K
+        self.R = R
+        
+    def solve(self,data_par:Patch,reg_par:Patch):
+        data_par = data_par.map_to_img(self.data)
+        reg_par = reg_par.map_to_img(self.data[:-1,:-1])
+        l2 = SDL2(b=self.data.ravel(),sigma=data_par.ravel())
+        l21 = SDL21(ndim=2,sigma=reg_par.ravel())
+        tau = 1.0 / np.sqrt(self.R)
+        mu = 1.0 / (tau*self.R)
+        rec = PrimalDual(l2,l21,self.K,tau=tau,mu=mu,theta=1.0,x0=np.zeros_like(self.data.ravel()),niter=1000)
+        return rec.reshape(self.data.shape)
